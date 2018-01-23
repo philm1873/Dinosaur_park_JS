c91d9d9f7fc5d2f54a49aae2a0187d63c439cfad
Phil Mitchell <pm1873@gmail.com>
2018-01-23 18:35:20 +0000

    all tests passed

--- park.js
+++ park.js
@@ -14,4 +14,17 @@ Park.prototype.removeTypeOfDinosaur = function(type){
   this.enclosure = this.enclosure.filter(dinosaur => dinosaur.type !== type);
 }
 
+Park.prototype.getAllDinosaursByOffspringRate = function(rate){
+  dinosaurs = this.enclosure.filter(dinosaur => dinosaur.numberOffspring > 2);
+  return dinosaurs;
+}
+
+Park.prototype.calculateDinosaurs = function(years){
+  var totalDinosaurs = 0;
+  for (dinosaur of this.enclosure){
+    totalDinosaurs += (1 + (dinosaur.numberOffspring)) ** years;
+  }
+  return totalDinosaurs;
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -3,12 +3,12 @@ const Park = require('../park.js');
 const Dinosaur = require('../dinosaur.js');
 
 describe('Park', function(){
-  let park;
+  let park, dinosaurOne, dinosaurTwo, dinosaurThree;
 
   beforeEach(function(){
     park = new Park();
     dinosaurOne = new Dinosaur('Tyrannosaurus', 3);
-    dinosaurTwo = new Dinosaur('Velociraptor', 4);
+    dinosaurTwo = new Dinosaur('Velociraptor', 2);
     dinosaurThree = new Dinosaur('Tyrannosaurus', 3);
   })
 
@@ -28,4 +28,27 @@ describe('Park', function(){
     park.removeTypeOfDinosaur('Tyrannosaurus');
     assert.strictEqual(park.enclosureSize(), 1);
   })
+
+  it('Can find all dinosaurs by offspring rate', function(){
+    park.addDinosaur(dinosaurOne);
+    park.addDinosaur(dinosaurTwo);
+    park.addDinosaur(dinosaurThree);
+    assert.strictEqual(park.getAllDinosaursByOffspringRate().length, 2)
+  })
+
+  it('should be able to calculate number of dinosaurs after 1 year starting with 1 dinosaur', function(){
+    park.addDinosaur(dinosaurOne);
+    assert.strictEqual(park.calculateDinosaurs(1), 4);
+  });
+
+  it('should be able to calculate number of dinosaurs after 2 years starting with 1 dinosaur', function(){
+    park.addDinosaur(dinosaurOne);
+    assert.strictEqual(park.calculateDinosaurs(2), 16);
+  });
+
+  it('should be able to calculate number of dinosaur after year two starting with 2 dinosaurs', function(){
+    park.addDinosaur(dinosaurOne);
+    park.addDinosaur(dinosaurTwo);
+    assert.strictEqual(park.calculateDinosaurs(2), 25);
+  });
 })
