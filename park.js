e1f858cbb1729ae507596722cdbbafde44be2d0e
Phil Mitchell <pm1873@gmail.com>
2018-01-23 16:42:12 +0000

    Added remove dinosaur by type method to park

--- park.js
+++ park.js
@@ -6,4 +6,12 @@ Park.prototype.enclosureSize = function(){
   return this.enclosure.length;
 }
 
+Park.prototype.addDinosaur = function(dinosaur){
+  this.enclosure.push(dinosaur);
+}
+
+Park.prototype.removeTypeOfDinosaur = function(type){
+  this.enclosure = this.enclosure.filter(dinosaur => dinosaur.type !== type);
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,14 +1,31 @@
 const assert = require('assert');
 const Park = require('../park.js');
+const Dinosaur = require('../dinosaur.js');
 
 describe('Park', function(){
   let park;
 
   beforeEach(function(){
     park = new Park();
+    dinosaurOne = new Dinosaur('Tyrannosaurus', 3);
+    dinosaurTwo = new Dinosaur('Velociraptor', 4);
+    dinosaurThree = new Dinosaur('Tyrannosaurus', 3);
   })
 
   it('Park starts empty', function(){
     assert.strictEqual(park.enclosureSize(), 0);
+  })
+
+  it('Park can add dinosaur to enclosure', function(){
+    park.addDinosaur(dinosaurOne);
+    assert.strictEqual(park.enclosureSize(), 1);
+  })
+
+  it('Can remove all dinosaurs of particular type', function(){
+    park.addDinosaur(dinosaurOne);
+    park.addDinosaur(dinosaurTwo);
+    park.addDinosaur(dinosaurThree);
+    park.removeTypeOfDinosaur('Tyrannosaurus');
+    assert.strictEqual(park.enclosureSize(), 1);
   })
 })
